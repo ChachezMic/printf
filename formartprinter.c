ab9dd7afb606e69fa8726f00bc145d31767cff99
ChachezMic <chachemic@gmail.com>
2023-09-20 14:32:09 -0700

    printf.c

---
+++ formartprinter.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <stdarg.h>
+
+int my_printf(const char *format, ...)
+{
+	int count = 0;
+
+	va_list lists;
+	va_start(lists, format);
+	while (*format != '\0')
+	{
+		if (*format == '%')
+		{
+			format++;
+			switch (*format)
+				{
+					case 'c':
+						{
+							char c = va_arg(lists, int);
+							putchar(c);
+							count += 1;
+							break;
+						}
+					case 's':
+						{
+							char *str = va_arg(lists, char *);
+							while (*str != '\0')
+							{
+								putchar(*str);
+								str++;
+								count += 1;
+								break;
+							}
+							case '%':
+							{
+								putchar('%');
+								count += 1;
+								break;
+							}
+							default:
+							 printf("Invalid format specifier: %c", *format);
+							 count += 1;
+							 break;
+						}
+				}
+		}
+		else
+			{
+				putchar(*format);
+				 count += 1;
+			}
+			format++;
+		}
+		va_end(lists);
+		return count;
+}
+
